ce5137ba3dca299800cfd8e54dbb664b7e9c0744
Mike Lin <mlin@mlin.net>
2019-02-18 00:57:33 -0800

    wip

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 set -ex -o pipefail
 
-if [ "$CI" == "true" ] && [ "$TRAVIS" == "true" ]; then
-    apt-get update
-    apt-get install -y python3-pip python3-setuptools
-    pip3 install miniwdl
-    export PATH=$PATH:$HOME/.local/bin
-fi
-
 ORIG_CWD="$(pwd)"
 cd "$(dirname $0)/ABOlocus"
 miniwdl check vg_ABOlocus_test.wdl 
