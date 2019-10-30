172e25658312d7d91c77f6818e95c4006b4ea9dd
Charles Markello <cmarkell@ucsc.edu>
2019-10-29 20:29:19 -0400

    Cleanup unit tests. Remove debug flags.

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -5,15 +5,15 @@ TRAVIS_DEBUG_MODE=true
 ORIG_CWD="$(pwd)"
 cd "$(dirname $0)/ABOlocus"
 miniwdl check --no-quant-check vg_ABOlocus_test_small.wdl
-miniwdl cromwell --debug --no-quant-check -d "$ORIG_CWD/vg_wdl_tests_ABOlocus_small" \
+miniwdl cromwell --no-quant-check -d "$ORIG_CWD/vg_wdl_tests_ABOlocus_small" \
     vg_ABOlocus_test_small.wdl \
     ABOlocus_fa_gz=ABOlocus.fa.gz ABOlocus_small_vcf_gz=ABOlocus_small.vcf.gz reads_bam=HG01308.ABOlocus.bam
-#miniwdl check --no-quant-check vg_ABOlocus_test_SV.wdl
-#miniwdl cromwell --no-quant-check -d "$ORIG_CWD/vg_wdl_tests_ABOlocus_SV" \
-#    vg_ABOlocus_test_SV.wdl \
-#    ABOlocus_fa_gz=ABOlocus.fa.gz ABOlocus_SV_vcf_gz=ABOlocus_SV.vcf.gz reads_bam=HG01308.ABOlocus.bam
+miniwdl check --no-quant-check vg_ABOlocus_test_SV.wdl
+miniwdl cromwell --no-quant-check -d "$ORIG_CWD/vg_wdl_tests_ABOlocus_SV" \
+    vg_ABOlocus_test_SV.wdl \
+    ABOlocus_fa_gz=ABOlocus.fa.gz ABOlocus_SV_vcf_gz=ABOlocus_SV.vcf.gz reads_bam=HG01308.ABOlocus.bam
 miniwdl check --no-quant-check vg_ABOlocus_test_pedigree.wdl
-miniwdl cromwell --debug --no-quant-check -d "$ORIG_CWD/vg_wdl_tests_ABOlocus_pedigree" \
+miniwdl cromwell --no-quant-check -d "$ORIG_CWD/vg_wdl_tests_ABOlocus_pedigree" \
     vg_ABOlocus_test_pedigree.wdl \
     ABOlocus_fa_gz=ABOlocus.fa.gz ABOlocus_small_vcf_gz=ABOlocus_small.vcf.gz \
     maternal_reads_bam=HG004.hs37d5.2x250.abo.bam \
